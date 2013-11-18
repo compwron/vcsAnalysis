4b1b0f97d41390b1f7eb2121d86c94d55dfd20f6
compwron <wordymusic@gmail.com>
2013-11-18 01:53:26 -0800

    formatting

--- lib/commits.rb
+++ lib/commits.rb
@@ -2,7 +2,6 @@ class Commits
   include Enumerable
   require_relative 'commit'
 
-  # Commits.new(Parser.new(opts[:root], opts[:after]).lines, opts[:devnames], opts[:cardprefix])
   def initialize lines, dev_names, card_prefix
     @commits = lines.map { |line| Commit.new(line, devs_in(line, dev_names), card_in(line, card_prefix)) }
   end
